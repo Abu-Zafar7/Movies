75eb49726b52382394e3df038c42c52f5ab8de03
casper-d07 <abuzafars99@gmail.com>
2023-01-02 11:23:45 +0530

    Initial Commit

--- movies/urls.py
+++ movies/urls.py
@@ -20,5 +20,10 @@ from movies import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homepage),
-    path('movies/', views.movies)
+    path('movies/', views.movies),
+    
+    path('movies/<int:id>',views.detail),
+    path('movies/add',views.add),
+    path('movies/delete/<int:id>',views.delete)
+    
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,28 +1,41 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.http.response import Http404
 from django.shortcuts import render
+from .models import Movie
 
-data = {"movies":[
-    {
-      "id": 1,
-      "title": "Dabangg",
-      "year": 2011
-    },
-    {
-       "id": 2,
-      "title": "Wanted",
-      "year": 2013
-    },
-    {
-      "id": 1,
-      "title": "DDLJ",
-      "year": 1999
-    }   
-]
-}
+
 
 def movies(request):
-    return render(request,"movies\movies.html", data)
+
+    data = Movie.objects.all()
+    return render(request,"movies\movies.html", {'movies':data})
 
 
 def homepage(request):
     return HttpResponse("HOME PAGE")
+
+def detail(request,id):
+  data = Movie.objects.get(pk = id)
+  return render(request,'movies/detail.html',{'movie':data})  
+
+
+def add(request):
+  title = request.POST.get('title')
+  year = request.POST.get('year')
+
+  if title and year:
+    movie = Movie(title = title, year = year)
+    movie.save()
+    return HttpResponseRedirect('/movies')
+  return render(request,'movies/add.html')
+
+def delete(request,id):
+  
+
+  try:
+      movie = Movie.objects.get(pk = id)
+  except:
+    raise Http404('Movie not found')    
+  movie.delete()  
+
+  return HttpResponseRedirect('/movies')
